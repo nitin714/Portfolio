7943e273cb8fdec7abac38687a6972d2884cd055
Nitin Vadhiya <nitinvadhiya123@gmail.com>
2022-07-13 11:33:44 +0530

    modified

--- components/SkillItem.js
+++ components/SkillItem.js
@@ -3,20 +3,12 @@ import Image from "next/image";
 
 function SkillItem({ img, title }) {
   return (
-    <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-      <div className="grid grid-cols-2 gap-4 justify-center items-center">
-        <div className="hidden md:flex m-auto">
-          <Image
-            src={img}
-            width="64px"
-            height="64px"
-            alt="/"
-            objectFit="contain"
-          />
-        </div>
-        <div className="flex break-words">
-          <h3>{title}</h3>
-        </div>
+    <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 grid md:grid-cols-2 gap-4 justify-center items-center">
+      <div className="hidden md:flex justify-center">
+        <Image src={img} width="64" height="64" alt="/" objectFit="contain" />
+      </div>
+      <div className="break-words justify-center">
+        <h3>{title}</h3>
       </div>
     </div>
   );
